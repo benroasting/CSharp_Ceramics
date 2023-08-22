bbee80cf73c97f41c15063247820e077b35fd32b
Ben Young <developing.ben@gmail.com>
2023-08-22 14:31:24 -0500

    set-up for ListView1

---
+++ ListView1/ListView1/ListView1/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListView1
+{
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        public static AppModels AppModels { get; } = new AppModels();
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ ListView1/ListView1/ListView1/AppModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ListView1
+{
+	public class AppModels
+	{
+		public ObservableCollection<Pokemon> Species { get; } = new ObservableCollection<Pokemon>
+		{
+			new Pokemon
+			{ Name = "Bulbasaur", Description = "", ImageSource = "" },
+			new Pokemon
+			{ Name = "Wobbuffet", Description = "", ImageSource = "" },
+
+		};
+
+		public void ReleasePokemon(Pokemon pokemon)
+		{
+			this.Species.Remove(pokemon);
+		}
+
+		public void CatchPokemon(Pokemon pokemon)
+		{
+
+		}
+
+    }
+}
+
