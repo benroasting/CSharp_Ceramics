6a78cfaf2f9ad2ae0202c5248b03cd2288677172
Ben Young <developing.ben@gmail.com>
2023-08-30 13:13:49 -0500

    ListView1 complete

--- ListView1/ListView1/ListView1/AppModels.cs
+++ ListView1/ListView1/ListView1/AppModels.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ListView1
 {
+
 	public class AppModels
 	{
-		public ObservableCollection<Pokemon> Species { get; } = new ObservableCollection<Pokemon>
+
+        public ObservableCollection<Pokemon> Pokemons { get; } = new ObservableCollection<Pokemon>
 		{
 			new Pokemon
 			{ Name = "Bulbasaur", Pokedex="001", Description="There is a plant seed on its back right from the day this Pokémon is born.", ImageSource="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" },
@@ -17,15 +20,16 @@ namespace ListView1
 			{ Name = "Sableye", Pokedex="302", Description="It dwells in the darkness of caves. It uses its sharp claws to dig up gems to nourish itself.", ImageSource="https://assets.pokemon.com/assets/cms2/img/pokedex/full/302.png"}
 		};
 
-		public void ReleasePokemon(Pokemon pokemon)
-		{
-			this.Species.Remove(pokemon);
-		}
+        public void ReleasePokemon(Pokemon pokemon)
+        {
+            Pokemons.Remove(pokemon);
+        }
 
-		public void CatchPokemon(Pokemon pokemon)
-		{
-			this.Species.Add(pokemon);
-		}
+
+        public void CatchPokemon(Pokemon pokemon)
+        {
+            Pokemons.Add(pokemon);
+        }
 
     }
 }
--- ListView1/ListView1/ListView1/MainPage.xaml.cs
+++ ListView1/ListView1/ListView1/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace ListView1
@@ -14,23 +15,23 @@ namespace ListView1
 
         public MainPage()
         {
-            this.InitializeComponent();
-            this.model = App.AppModels;
-            this.BindingContext = this.model;
-
+            InitializeComponent();
+            model = new AppModels();
+            BindingContext = model;
         }
 
         private void ReleasePokemon(object sender, SelectedItemChangedEventArgs e)
         {
-            this.model.ReleasePokemon(e.SelectedItem as Pokemon);
+            model.ReleasePokemon(e.SelectedItem as Pokemon);
         }
 
         private void CatchPokemon(object sender, EventArgs e)
         {
-            this.model.CatchPokemon(
+            model.CatchPokemon(
                 new Pokemon
                 { Name = "Sableye", Pokedex = "302", Description = "It dwells in the darkness of caves. It uses its sharp claws to dig up gems to nourish itself.", ImageSource = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/302.png" });
         }
+
     }
 }
 
