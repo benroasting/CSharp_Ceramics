9733a37263518967df1dde1a27db7e1aef4af932
Ben Young <developing.ben@gmail.com>
2023-08-30 14:31:43 -0500

    setup for Collection1

---
+++ Collection1/Collection1/Collection1/DeskItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Collection1
+{
+	public class DeskItem
+	{
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public string ImageSource { get; set; }
+
+		public ObservableCollection<DeskItem> ContainedItems { get; set; } = new ObservableCollection<DeskItem>();
+    }
+}
+
